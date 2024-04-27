d61dbaab47929cd23b5ac31d64545c5bca9ffa3b
smallc <2628035541@qq.com>
2024-04-27 19:17:01 +0800

    实现功能栏前端页面

---
+++ frontend/src/components/SideMenu.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="menu">
+      <a-menu mode="vertical" :default-selected-keys="['Editor']" @menu-item-click="changeComponent">
+        <a-menu-item key="资源管理器">资源管理器</a-menu-item>
+        <a-menu-item key="Editor">Editor</a-menu-item>
+        <a-menu-item key="PDF">PDF</a-menu-item>
+        <a-menu-item key="4">新功能</a-menu-item>
+      </a-menu>
+    </div>
+  </template>
+  <style scoped>
+  .menu {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 10px;
+  }
+  </style>
+
+<script>
+export default {
+    methods: {
+        changeComponent(key) {
+            //console.log(key);
+            this.$emit('componentChange', key);
+        },
+    }
+}
+</script>
+  
