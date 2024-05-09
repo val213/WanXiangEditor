79ffea8e86e2326dc394ee29199e061d10d2d380
smallc <2628035541@qq.com>
2024-05-10 01:30:33 +0800

    111

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -83,55 +83,54 @@
             }
 
             const onSelect = async (selectedKeys, data) => {
-              client.logger.info('onSelect',selectedKeys, data);
-            // 调用接口返回该文件的内容
-            const selectedKey = data.node.key;
-            const selectedTitle = data.node.title;
-            console.log(selectedTitle);
-            // 根据selectedTitle的文件名后缀判断文件类型
-            let parts = selectedTitle.split(".");
-            let extension = parts.pop();
-            // 判断文件类型是文本文件格式, 就调用接口获取文件内容
-            if (extension === 'txt' || extension === 'md' || extension === 'json' || extension === 'js' || extension === 'html' || extension === 'css' || extension === 'py' || extension === 'java' || extension === 'c' || extension === 'cpp' || extension === 'h' || extension === 'hpp' || extension === 'cs' || extension === 'go' || extension === 'php' || extension === 'sql' || extension === 'sh' || extension === 'bat' || extension === 'xml' || extension === 'yaml' || extension === 'yml' || extension === 'ini' || extension === 'conf' || extension === 'cfg' || extension === 'log' || extension === 'properties' || extension === 'gradle' ||extension ==='json') {
+              client.logger.info('onSelect', selectedKeys, data);
+              // 调用接口返回该文件的内容
+              const selectedKey = data.node.key;
+              const selectedTitle = data.node.title;
+              // 根据selectedTitle的文件名后缀判断文件类型
+              let parts = selectedTitle.split(".");
+              let extension = parts.pop();
+              // 判断文件类型是文本文件格式, 就调用接口获取文件内容
+              if (extension === 'txt' || extension === 'md' || extension === 'json' || extension === 'js' || extension === 'html' || extension === 'css' || extension === 'py' || extension === 'java' || extension === 'c' || extension === 'cpp' || extension === 'h' || extension === 'hpp' || extension === 'cs' || extension === 'go' || extension === 'php' || extension === 'sql' || extension === 'sh' || extension === 'bat' || extension === 'xml' || extension === 'yaml' || extension === 'yml' || extension === 'ini' || extension === 'conf' || extension === 'cfg' || extension === 'log' || extension === 'properties' || extension === 'gradle' || extension === 'pdf') {
                 0
-            }
-            else {
+              }
+              else {
                 // 如果不是文本文件格式, 就提示用户不支持该文件类型
                 client.logger.info('不支持该文件类型', selectedTitle);
                 // 弹出提示框
                 Modal.error({
-                    title: '不支持该文件类型,请选择文本格式文件~',
-                    content: '该文件不会显示在文本编辑器中，因为它要么是二进制文件，要么使用不受支持的文本编码。',
+                  title: '不支持该文件类型,请选择文本格式文件~',
+                  content: '该文件不会显示在文本编辑器中，因为它要么是二进制文件，要么使用不受支持的文本编码。',
                 });
-            }
-            // 处理文件名->文件路径
-            const GetFilePath =(data)=>{
+              }
+              // 处理文件名->文件路径
+              const GetFilePath = (data) => {
                 // 递归获取文件路径
                 let filepath = '';
                 let parent = data.node;
                 while (parent) {
-                    filepath = parent.title + '/' + filepath;
-                    parent = parent.parent;
+                  filepath = parent.title + '/' + filepath;
+                  parent = parent.parent;
                 }
                 // 去掉最后一个'/'
                 filepath = filepath.substring(0, filepath.length - 1);
                 return filepath;
-            };
-            let filePath = GetFilePath(data);
-            client.logger.info('文件路径', filePath);
-            // 将被选中文件节点的title作为参数去请求接口
-            let ret = await client.callApi('SelectFile', {selectedTitle: selectedTitle, filePath: filePath, fileType: extension});
-            if (ret.isSucc) {
+              };
+              let filePath = GetFilePath(data);
+              client.logger.info('文件路径', filePath);
+              // 将被选中文件节点的title作为参数去请求接口
+              let ret = await client.callApi('SelectFile', { selectedTitle: selectedTitle, filePath: filePath, fileType: extension });
+              if (ret.isSucc) {
                 client.logger.info('成功获取文件内容', ret);
-                    let txtData = ret.res.content;
-                    // 将 selectedTitle 和 txtData 传给 tabs 组件
-                    client.logger.info('开始传递文件内容', selectedTitle, txtData);
-                    this.$emit('file-selected', selectedTitle, txtData);
-                }
-                else {
+                let txtData = ret.res.content;
+                // 将 selectedTitle 和 txtData 传给 tabs 组件
+                client.logger.info('开始传递文件内容', selectedTitle, txtData);
+                context.emit('file-selected', selectedTitle, txtData, extension);
+              }
+              else {
                 // 处理错误
-                client.logger.error('获取文件内容失败', ret.res);   
-            }
+                client.logger.error('获取文件内容失败', ret.res);
+              }
         }
             return {
                 searchKey,
