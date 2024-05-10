3297d7f718b19900d164f3c2a7ccd552ac200bfa
wuyong065 <2609947331@qq.com>
2024-05-10 09:57:10 +0800

    实现了显示个人信息，优化了登录操作（二次确定，界面优化）

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -261,7 +261,6 @@ export default {
     },
     handleLoginSuccess(username){
         this.currentUsername = username;
-        console.log("currentUsername是" + this.currentUsername);
     }
   },
 };
